e7f592e7a190b7dc3b5e34156a35583061339b2d
Sara <sarah02_ardito@hotmail.it>
2023-05-02 09:31:53 +0200

    added position to variables

--- code/num_simulation.py
+++ code/num_simulation.py
@@ -2,12 +2,14 @@
 This file uses Euler method to solve the system of equation:
     d_t ( w ) = -b/J ( w ) + Kt/J ( i )
     d_t ( i ) = -Ke/L ( w ) - R/L ( i ) + V/L
+    d_t ( theta ) = ( w )
 that can be written as follows
     d_t ( u ) = A ( u ) + B
-where A = [-b/J   Kt/J]  and  B = [0  ]
-          [-Ke/L  -R/L]           [V/L]
+where A = [-b/J   Kt/J  0]  and  B = [0  ]
+          [-Ke/L  -R/L  0]           [V/L]
+          [1        0   0]           [0  ]
 
-Note: the inizial condition are w=0, i=0
+Note: the inizial condition are w=0, i=0, theta=0
 """
 
 import os
@@ -30,8 +32,8 @@ def euler_step(u, F, dt, *params):
 #--------------------------Right Hand Side------------------------#
 def F(u, J, b, Kt, Ke, R, L, V):
     #define A and B
-    A = np.array([[-b/J, Kt/J], [-Ke/L, -R/L]])     #matrix A of the system
-    B = np.array((0, V/L))
+    A = np.array([[-b/J, Kt/J, 0], [-Ke/L, -R/L, 0], [1, 0, 0]])     #matrix A of the system
+    B = np.array((0, V/L, 0))
     return A @ u + B
 
 #----------------------------Step function------------------------#
@@ -44,25 +46,27 @@ def evolution(int_method, Nt_step, dt, physics_params, V, t0):
     #-----------------Initialize the problem-------------------#
     tmax = dt * Nt_step             #total time of simulation
     tt = np.arange(0, tmax, dt)     #temporal grid
-    u0 = np.array((0, 0))           #initial condition
+    u0 = np.array((0, 0, 0))           #initial condition
     u_t = np.copy(u0)               #create a copy to evolve it in time
     V_signal = V(tt, t0)            #signal applied to the system in time
     #----------------------------------------------------------#
 
     #----------------------Time evolution----------------------#
-    w = []  #initialize list of w value
-    i = []  #initialize list of i value
+    w = []      #initialize list of w value
+    i = []      #initialize list of i value
+    theta = []  #initialize list of theta value
     for Vi in V_signal:
         u_t = int_method(u_t, F, dt, *physics_params, Vi) #step n+1
         w.append(u_t[0])
         i.append(u_t[1])
-    return tt, np.array(w), np.array(i)
+        theta.append(u_t[2])
+    return tt, np.array(w), np.array(i), np.array(theta)
 
 
 if __name__ == '__main__':
     #Parameters of the simulation
-    Nt_step = 500     #temporal steps
-    dt = 1e-2         #temporal step size
+    Nt_step = 2e2     #temporal steps
+    dt = 1e-3         #temporal step size
     #Parameters of the system
     J = 0.01    #rotor's inertia [kg m^2]
     b = 0.001   #viscous friction coefficient [N m s]
@@ -78,18 +82,18 @@ if __name__ == '__main__':
     #Simulation
     physical_params = [J, b, Kt, Ke, R, L]
     simulation_params = [euler_step, Nt_step, dt]
-    tt, w, i = evolution(*simulation_params, physical_params, V, t0)
+    tt, w, i, theta = evolution(*simulation_params, physical_params, V, t0)
 
     #--------------------------Plot results----------------------#
-    plt.title('Transient response for DC motor')
+    plt.title('Step response for DC motor (open loop)')
     plt.xlabel('Time [s]')
-    plt.ylabel('i [A]')
+    plt.ylabel('$\Theta$ [rad]')
     plt.grid(True)
     plt.minorticks_on()
-    plt.plot(tt, w, linestyle='-', linewidth=1.8, marker='')
+    plt.plot(tt, theta, linestyle='-', linewidth=1.8, marker='')
 
     # save the plot in the results dir
-    out_name = os.path.join(results_dir, "StepResp_w_numSim.png")
+    out_name = os.path.join(results_dir, "StepResp_theta_numSim1.png")
     plt.savefig(out_name)
     plt.show()
 
